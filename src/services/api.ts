a58194c43ab963b3d033741271d41463c3619154
Everton Pinheiro <44790240+TIEverton@users.noreply.github.com>
2021-03-18 13:39:46 -0300

    Changing baseurl to production

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://json-server-challenge1.herokuapp.com',
   
-  // baseURL: 'http://localhost:3333',
+  // 'http://localhost:3333',
 });
